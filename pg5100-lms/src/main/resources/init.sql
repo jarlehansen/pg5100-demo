bb911abd294f671a0230c3a1241b6782e4c451dd
Jarle Hansen <jarle@jarlehansen.net>
2015-11-05 00:12:14 +0100

    start lms project

---
+++ pg5100-lms/src/main/resources/init.sql
@@ -0,0 +1,14 @@
+-- INSERT INTO User (id, email, password, usertype) VALUES (1, 'mari@test.com', '123ABc', 'STUDENT');
+-- INSERT INTO User (id, email, password, usertype) VALUES (2, 'ole@test.com', 'abC123', 'TEACHER');
+-- INSERT INTO User (id, email, password, usertype) VALUES (3, 'kari@test.com', '1234Abc', 'STUDENT');
+--
+-- INSERT INTO Location (id, building, room) VALUES (1, 'Galleriet', '82');
+-- INSERT INTO Location (id, building, room) VALUES (2, 'Galleriet', '81');
+--
+-- INSERT INTO Subject (id, name, FK_LOCATION) VALUES (1, 'PG5100', 1);
+-- INSERT INTO Subject (id, name, FK_LOCATION) VALUES (2, 'PG6100', 2);
+--
+-- INSERT INTO USR_SUB (SUBJECTS_ID, USERS_ID) VALUES (1, 1);
+-- INSERT INTO USR_SUB (SUBJECTS_ID, USERS_ID) VALUES (2, 1);
+-- INSERT INTO USR_SUB (SUBJECTS_ID, USERS_ID) VALUES (1, 2);
+-- INSERT INTO USR_SUB (SUBJECTS_ID, USERS_ID) VALUES (2, 3);
