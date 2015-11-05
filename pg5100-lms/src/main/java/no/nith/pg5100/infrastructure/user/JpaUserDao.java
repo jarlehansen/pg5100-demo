e158d5c96292f32ea03568c55a667e6be197f692
Jarle Hansen <jarle@jarlehansen.net>
2015-11-05 09:57:38 +0100

    location

--- pg5100-lms/src/main/java/no/nith/pg5100/infrastructure/user/JpaUserDao.java
+++ pg5100-lms/src/main/java/no/nith/pg5100/infrastructure/user/JpaUserDao.java
@@ -2,31 +2,55 @@ package no.nith.pg5100.infrastructure.user;
 
 import no.nith.pg5100.dto.User;
 
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
 import java.util.List;
 
+@JpaUser
+@Stateless
 public class JpaUserDao implements UserDao {
+
+    @PersistenceContext(unitName = "pg5100-lms")
+    private EntityManager entityManager;
+
+    public JpaUserDao() {
+    }
+
+    JpaUserDao(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
     @Override
     public User persist(User user) {
-        return null;
+        entityManager.persist(user);
+        return user;
     }
 
     @Override
     public boolean update(User user) {
-        return false;
+        if (!entityManager.contains(user)) {
+            entityManager.merge(user);
+        }
+
+        return true;
     }
 
     @Override
     public User findById(int id) {
-        return null;
+        return entityManager.find(User.class, id);
     }
 
     @Override
     public List<User> getAll() {
-        return null;
+        TypedQuery<User> query = entityManager.createNamedQuery("User.getAll", User.class);
+        return query.getResultList();
     }
 
     @Override
     public boolean remove(User user) {
-        return false;
+        entityManager.remove(user);
+        return true;
     }
 }
